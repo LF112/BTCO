44b8b8f5377b1d513a0bf1c98dfc3e653d7a585e
“LF112” <lf@lf112.net>
2019-04-12 17:48:13 +0800

    导入搜寻
    修复语法错误
    新增导入搜寻

--- BTCO/btco_main.py
+++ BTCO/btco_main.py
@@ -26,12 +26,26 @@ class btco_main:
     __BtcoPath = '/www/server/panel/plugin/btco' #BTCO 主体路径
     __SitePath = '/www/server/panel/BTPanel/templates/default' #宝塔面板模板路径
     __BtcoNick = 'btco.html' #BTCO 网页文件名
+    __BT_init = '/www/server/panel/BTPanel/__init__.py' #宝塔面板核心文件
+    BtcoIns = []
 
     # 实例化BTCO
     def __init__(self):
-        if os.path.exists(self.__SitePath)
-            if not os.path.isfile(self.__SitePath + self.__BtcoNick)
+        if os.path.exists(self.__SitePath):
+            if not os.path.isfile(self.__SitePath + self.__BtcoNick)：
                 public.ExecShell('cp -rf  %s' % self.__BtcoPath + '/Main/' + self.__BtcoNick +' '+ self.__SitePath)
+
+    def BtcoInstall(self, get):
+        initobj = open(self.__BT_init,'r')
+        for initLine in initobj:
+            for btcoin in ['@app.route(\'/btco\'']:
+                if btcoin.upper() in initLine.upper():
+                    self.BtcoIns.append(btcoin)
+            if len(self.BtcoIns) != 0:
+                initobj.close()
+                return public.returnMsg(True, 'Rua!')
+        initobj.close()
+        return public.returnMsg(True, 'Not Found!')
 
     def BTCO(self, get):
         return public.returnMsg(True, 'Rua!')
