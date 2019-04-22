95e9c7d07e270e475ea9bdf681551fea797eff9d
“LF112” <lf@lf112.net>
2019-04-22 21:15:16 +0800

    给主要JS增加版权注释

--- BTCO/static/script/btco.app.js
+++ BTCO/static/script/btco.app.js
@@ -1,3 +1,8 @@
+// Javascript BTCO - BT-Panel Responsive solution.
+// Released under GPL-3.0 License Opensourced. (https://github.com/LF112/BTCO/blob/master/LICENSE)
+// Author @LF112 (https://github.com/LF112)
+// Copyright reservation is required.
+
 window.onload = function(){
     console.clear();
     console.log('\n %c 🎉BTCO %c https://github.com/LF112/BTCO %c BY%c LF112  \n\n', 'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;', 'background:rgba(197, 197, 197, 0.89); padding:5px 0;', 'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;', 'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;');
