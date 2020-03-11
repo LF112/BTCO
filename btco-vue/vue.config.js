9396f2dcb9cf7b756dbcaccaa38bd84b8486c4dc
futiwolf <lf@lf112.net>
2020-03-11 10:50:55 +0800

    本体 Build 后转移文件

--- btco-vue/vue.config.js
+++ btco-vue/vue.config.js
@@ -1,9 +1,45 @@
 const path = require('path'),
+    fs = require('fs'),
     resolve = (dir) => {
         return path.join(__dirname, dir)
     }
 
+var COPY = (src, dist, callback) => {
+    const _copy = (err, src, dist) => {
+        if (err)
+            callback(err)
+        else {
+            fs.readdir(src, function (err, paths) {
+                if (err)
+                    callback(err)
+                else {
+                    paths.forEach(function (path) {
+                        let _src = src + '/' + path,
+                            _dist = dist + '/' + path
+                        fs.stat(_src, function (err, stat) {
+                            if (err)
+                                callback(err)
+                            else {
+                                if (stat.isFile())
+                                    fs.writeFileSync(_dist, fs.readFileSync(_src));
+                                else if (stat.isDirectory()) COPY(_src, _dist, callback)
+                            }
+                        })
+                    })
+                }
+            })
+        }
+    }
+    fs.access(dist, function (err) {
+        if (err) fs.mkdirSync(dist)
+        _copy(null, src, dist)
+    })
+}
+
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '././' : '/',
+    assetsDir: '././static/',
+    productionSourceMap: true,
     chainWebpack(config) {
         config.module
             .rule('svg')
@@ -20,5 +56,33 @@ module.exports = {
                 symbolId: 'BTCO-[name]'
             })
             .end()
+    },
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+            config.plugins.push({
+                apply: (compiler) => {
+                    compiler.hooks.done.tap('btco', compilation => {
+                        console.log('[BTCO] 正在转移文件...')
+
+                        const PATH = path.resolve(__dirname, '..') + '/btco-vue'
+                        fs.mkdirSync(PATH + '/dist/templates')
+                        fs.rename(PATH + '/dist/index.html', PATH + '/dist/templates/index.html', (err) => {
+                            if (err) console.log('[BTCO] index.html 转移失败！')
+                            fs.rename(PATH + '/dist/favicon.ico', PATH + '/dist/templates/favicon.ico', (err) => {
+                                if (err) console.log('[BTCO] favicon.ico 转移失败！')
+                                else {
+                                    COPY('./dist', path.resolve(__dirname, '..') + '/BTCO', (e) => {
+                                        if (e) console.log('[BTCO] 转移失败！')
+                                        else console.log('[BTCO] 转移完成 √')
+                                    })
+                                }
+                            })
+                        })
+                    })
+                }
+            })
+        }
     }
 }
+
